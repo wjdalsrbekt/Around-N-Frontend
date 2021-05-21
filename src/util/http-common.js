fa38c995b4ca8a4fc190d6bc122f7f29834cb730
at8in <at8in@naver.com>
2021-05-21 09:29:25 +0900

    흠..

--- src/util/http-common.js
+++ src/util/http-common.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'http://localhost:9997/happyhouse',
   headers: {
+    'Access-Control-Allow-Origin': '*',
     'Content-type': 'application/json',
   },
 });
