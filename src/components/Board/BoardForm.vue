44a886da610f32812a44ad944f1c31b233fd5d59
정민규 <wjdalsrbekt@gmail.com>
2021-05-24 17:39:03 +0900

    0524

--- src/components/Board/BoardForm.vue
+++ src/components/Board/BoardForm.vue
@@ -200,7 +200,7 @@ export default {
       if (this.$cookie.get('userid') != this.userid) {
         alert('본인만 게시글을 수정할 수 있습니다.');
       } else {
-        http
+        /*http
           .put('/board', {
             bnum: this.bnum,
             title: this.title,
@@ -221,8 +221,49 @@ export default {
           })
           .catch(() => {
             this.moveList();
-          });
+          });*/
+           const formData = new FormData();
+      // formData.append('bnum', this.bnum);
+      formData.append('title', this.title);
+      formData.append('userid', this.$cookie.get('userid'));
+      formData.append('price', this.price);
+      formData.append('content', this.content);
+      formData.append('regdate', new Date());
+      formData.append('upfile', this.upfile);
+       http
+        .put(
+          '/board',
+          // {
+          //   bnum: this.bnum,
+          //   title: this.title,
+          //   // userid: this.userid,
+          //   userid: this.$cookie.get('userid'),
+          //   price: this.price,
+          //   content: this.content,
+          //   regdate: this.regdate,
+          //   upfile: this.upfile,
+          // },
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data; charset=utf-8;',
+            },
+          }
+        )
+        .then(({ data }) => {
+          let msg = '수정 처리 중 문제가 발생하였습니다.';
+          if (data === 'success') {
+            msg = '수정이 완료되었습니다.';
+          }
+
+          alert(msg);
+          this.moveList();
+        })
+        .catch(() => {
+          this.moveList();
+        });
       }
+      
     },
     moveList() {
       this.$router.push('/board');
