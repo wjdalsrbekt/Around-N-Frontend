661cf1bac3da91a7c0d72ccc1265cc010f3a2f59
at8in <at8in@naver.com>
2021-05-25 03:05:57 +0900

    탈퇴 스피너 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,15 @@
     <div>
       <h1>Main 화면</h1>
       <q-btn color="purple" @click="showLoading" />
+      <!--ajax bar -->
+      <q-ajax-bar ref="bar" position="bottom" color="lime-5" size="12px" skip-hijack />
+      <q-btn color="purple" label="탈퇴 스피너" @click="trigger" />
     </div>
   </div>
 </template>
 
 <script>
+import { QSpinnerGears, QSpinnerHourglass } from 'quasar';
 // @ is an alias to /src
 export default {
   name: 'Home',
@@ -21,6 +25,36 @@ export default {
         this.$q.loading.hide();
         this.timer = void 0;
       }, 2000);
+    },
+    trigger() {
+      const bar = this.$refs.bar;
+
+      this.$q.loading.show({
+        spinner: QSpinnerHourglass,
+        spinnerColor: 'lime-5',
+        messageColor: 'white',
+        message: '<p class="text-overline">정보를 삭제하는 중입니다...</p>',
+        delay: 200,
+        spinnerSize: '5em',
+      });
+
+      bar.start();
+
+      this.timer = setTimeout(() => {
+        if (this.$refs.bar) {
+          this.$refs.bar.stop();
+          this.$q.loading.show({
+            spinner: QSpinnerGears,
+            spinnerColor: 'deep-orange',
+            message: '이용해주셔서 감사합니다.',
+          });
+
+          this.timer = setTimeout(() => {
+            this.$q.loading.hide();
+            this.timer = void 0;
+          }, 1500);
+        }
+      }, 3000);
     },
   },
   beforeDestroy() {
