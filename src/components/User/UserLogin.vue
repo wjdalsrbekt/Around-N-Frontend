1e6fe0af3a9332230bce5880edb6ad7403e780a5
at8in <at8in@naver.com>
2021-05-25 10:55:01 +0900

    로그인 보수 + 메인

--- src/components/User/UserLogin.vue
+++ src/components/User/UserLogin.vue
@@ -1,72 +1,72 @@
 <template>
   <div class="q-pa-md">
-    <div style="max-width: 500px;margin:0 auto">
-    <q-card>
-      <br />
-      <p class="text-h5" for="userid">아이디</p>
-      <q-card-section>
-        <q-input
-          ref="userid"
-          type="text"
-          v-model="userid"
-          name="userid"
-          id="userid"
-          placeholder="아이디 입력"
-          lazy-rules
-          :rules="[(val) => (val && val.length > 0) || '필수입력 사항입니다.']"
-          label="아이디를 입력해주세요."
+    <div style="max-width: 500px; margin: 0 auto">
+      <q-card>
+        <br />
+        <p class="text-h5" for="userid">아이디</p>
+        <q-card-section>
+          <q-input
+            ref="userid"
+            type="text"
+            v-model="userid"
+            name="userid"
+            id="userid"
+            placeholder="아이디 입력"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || '필수입력 사항입니다.']"
+            label="아이디를 입력해주세요."
+          />
+          <br />
+        </q-card-section>
+        <p class="text-h5" for="userpwd">비밀번호</p>
+        <q-card-section>
+          <q-input
+            ref="userpwd"
+            type="password"
+            v-model="userpwd"
+            name="userpwd"
+            id="userpwd"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || '필수입력 사항입니다.']"
+            placeholder="비밀번호 입력"
+            label="비밀번호를 입력해주세요."
+          />
+        </q-card-section>
+        <br />
+
+        <q-btn
+          icon="login"
+          text-color="white"
+          label="로그인"
+          color="blue-grey-6"
+          class="text-bold"
+          @click="login"
         />
         <br />
-      </q-card-section>
-      <p class="text-h5" for="userpwd">비밀번호</p>
-      <q-card-section>
-        <q-input
-          ref="userpwd"
-          type="password"
-          v-model="userpwd"
-          name="userpwd"
-          id="userpwd"
-          lazy-rules
-          :rules="[(val) => (val && val.length > 0) || '필수입력 사항입니다.']"
-          placeholder="비밀번호 입력"
-          label="비밀번호를 입력해주세요."
-        />
-      </q-card-section>
-      <br />
-
-      <q-btn
-        icon="login"
-        text-color="white"
-        label="로그인"
-        color="blue-grey-6"
-        class="text-bold"
-        @click="login"
-      />
-      <br />
-      <br />
-    </q-card>
-    <q-dialog v-model="alert">
-      <q-card class="bg-blue-grey-9">
-        <q-card-section class="text-lime-5">
-          <div class="text-h6 text-bold">알림</div>
-        </q-card-section>
-        <q-card-section class="q-pt-none text-subtitle2 text-white"> {{ msg }}</q-card-section>
-        <q-card-actions align="right">
-          <q-btn flat label="OK" color="teal-3" @click="goList" />
-        </q-card-actions>
+        <br />
       </q-card>
-    </q-dialog>
-    <q-dialog v-model="alert2">
-      <q-card class="bg-blue-grey-9">
-        <q-card-section class="text-lime-5">
-          <div class="text-h6 text-bold">{{ userid }}님!</div>
-        </q-card-section>
-        <q-card-section class="q-pt-none text-subtitle2 text-white"> 환영합니다!</q-card-section>
-        <q-card-actions align="right">
-          <q-btn flat label="OK" color="teal-3" @click="goHome" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+      <q-dialog v-model="alert">
+        <q-card class="bg-blue-grey-9">
+          <q-card-section class="text-lime-5">
+            <div class="text-h6 text-bold">알림</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none text-subtitle2 text-white"> {{ msg }}</q-card-section>
+          <q-card-actions align="right">
+            <q-btn flat label="OK" color="teal-3" @click="goList" />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+      <q-dialog v-model="alert2">
+        <q-card class="bg-blue-grey-9">
+          <q-card-section class="text-lime-5">
+            <div class="text-h6 text-bold">{{ userid }}님!</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none text-subtitle2 text-white"> 환영합니다!</q-card-section>
+          <q-card-actions align="right">
+            <q-btn flat label="OK" color="teal-3" @click="goHome" />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
     </div>
   </div>
 </template>
@@ -126,6 +126,7 @@ export default {
     goHome() {
       this.alert2 = false;
       this.$router.push('/');
+      this.$router.go(this.$router.currentRoute);
     },
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <div>
-      <h1>Main 화면</h1>
-      <q-btn color="purple" @click="showLoading" />
-      <!--ajax bar -->
-      <q-ajax-bar ref="bar" position="bottom" color="lime-5" size="12px" skip-hijack />
-      <q-btn color="purple" label="탈퇴 스피너" @click="trigger" />
+  <div class="home q-pa-md">
+    <div class="row">
+      <q-parallax :src="image" :height="500" :speed="0.5">
+        <h1 class="text-white logo">Around:N</h1>
+      </q-parallax>
     </div>
+    <q-btn color="purple" @click="showLoading" />
+    <!--ajax bar -->
+    <q-ajax-bar ref="bar" position="bottom" color="lime-5" size="12px" skip-hijack />
+    <q-btn color="purple" label="탈퇴 스피너" @click="trigger" />
   </div>
 </template>
 
@@ -17,6 +18,11 @@ import { QSpinnerGears, QSpinnerHourglass } from 'quasar';
 export default {
   name: 'Home',
   components: {},
+  data() {
+    return {
+      image: require('../assets/main_logo.jpg'),
+    };
+  },
   methods: {
     showLoading() {
       this.$q.loading.show();
@@ -65,3 +71,8 @@ export default {
   },
 };
 </script>
+<style scoped>
+.logo {
+  text-shadow: 4px 4px 8px #1b233a, 0px 0px 2px white, 8px 8px 10px #ec9750;
+}
+</style>
